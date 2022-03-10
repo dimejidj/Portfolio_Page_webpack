ea36fa67b241118998e78609becc276292783f41
dimeji olatunde <dimeji_09@hotmail.co.uk>
2022-03-10 22:38:53 +0000

    webpack config adjusted

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
+  mode: "production",
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       title: "Portfolio Page",
     }),
   ],
+  devtool: "inline-source-map",
   output: {
-    filename: "index.bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
